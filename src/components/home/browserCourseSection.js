19ea9efcb73ad2354174ebc496214645eab33294
murabiyoon1 <info@murabiyoon.com>
2023-03-08 11:25:39 +0000

    Update browserCourseSection.js

--- src/components/home/browserCourseSection.js
+++ src/components/home/browserCourseSection.js
@@ -6,7 +6,12 @@ const BrowserCourseSection = () => {
         <div className="browser-course-area">
             <div className="container">
                 <div className="row">
-                    <div className="col-xl-6 col-lg-6 col-md-12 mb-30">
+                <div className="col-12">
+                        <div className="section-title text-center mb-60">
+                            <h1>Our School <span className="down-mark-line-2"> Settings</span></h1>
+                        </div>
+                    </div>
+                        <div className="col-xl-6 col-lg-6 col-md-12 mb-30">
                         <div className="browser-course-wrapper course-height">
                             <div className="browser-course-bg">
                                 <img src="/assets/img/browser-course/feature1.png" alt="img not found" />
