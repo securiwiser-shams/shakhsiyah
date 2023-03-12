79c3edf57138d96b6ce94213bcce30b0fd63392b
murabiyoon1 <info@murabiyoon.com>
2023-03-12 22:06:33 +0000

    update

--- src/components/home/partnerSection.js
+++ src/components/home/partnerSection.js
@@ -20,34 +20,34 @@ const PartnerSection = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-xl-7 col-lg-8 col-md-8">
+                    <div className="col-xl-7 col-lg-8 col-md-8"> 
                         <div className="partner-wrapper">
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/dfe.png" alt="not found" />
+                                <img src="/assets/img/brand/dfe.png" alt="not found" class="responsive" width="105" height="62" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/isi.png" alt="not found" />
+                                <img src="/assets/img/brand/isi.png" alt="not found" class="responsive" width="240" height="43" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/slough.png" alt="not found" />
+                                <img src="/assets/img/brand/slough.png" alt="not found" class="responsive" width="125" height="53" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/haringey.png" alt="not found" />
+                                <img src="/assets/img/brand/haringey.png" alt="not found" class="responsive" width="120" height="51" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/ams.png" alt="not found" />
+                                <img src="/assets/img/brand/isa.png" alt="not found" class="responsive" width="152" height="67" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/isa.png" alt="not found" />
+                                <img src="/assets/img/brand/ams.png" alt="not found" class="responsive" width="61" height="62" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/nida.png" alt="not found" />
+                                <img src="/assets/img/brand/nida.png" alt="not found" class="responsive" width="72" height="72" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/isa.png" alt="not found" />
+                                <img src="/assets/img/brand/ti.png" alt="not found" class="responsive" width="83" height="79" ></img>
                             </div>
                             <div className="singel-partner">
-                                <img src="/assets/img/brand/securiwiser.png" alt="not found" />
+                                <img src="/assets/img/brand/securiwiser.png" alt="not found" class="responsive" width="73" height="76" ></img>
                             </div>
                         </div>
                     </div>
