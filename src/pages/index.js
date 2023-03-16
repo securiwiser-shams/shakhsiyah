303a4070b2f4fff6761c86a0352d53a76b39adc2
murabiyoon1 <info@murabiyoon.com>
2023-03-16 16:39:41 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import { Seo } from "../components/seo"
+import Seo from "../components/layout/seo"
 
 export const query = graphql`
   query {
