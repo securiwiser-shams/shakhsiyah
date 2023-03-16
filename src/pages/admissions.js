66680082db561ca426c3b56a6e3fbf2195bcb04c
murabiyoon1 <info@murabiyoon.com>
2023-03-16 18:55:34 +0000

    update

--- src/pages/admissions.js
+++ src/pages/admissions.js
@@ -1,38 +1,16 @@
-import React from "react"
-import { graphql } from "gatsby"
+import * as React from "react"
 import Layout from "../components/layout"
+import { Seo } from "../components/seo"
 import BlogDetailsPageMain from "../components/admissions"
-import SEO from "../components/layout/seo"
 
-export const query = graphql`
-  query {
-    allFile(
-      filter: {sourceInstanceName: {eq: "seo-images"}, name: {in: "admissions"}}
-    ) {
-      edges {
-        node {
-          publicURL
-        }
-      }
-    }
-  }
-`
-const BlogDetailsPage = ({ data }) => {
-  const seoImageUrl = data?.allFile?.edges[0]?.node?.publicURL
-  return (
-    <Layout>
-      <SEO
-        image={seoImageUrl?.length > 1 ? `https://shakhsiyah.netlify.app${seoImageUrl}` : null}
-        description="Shakhsiyah Schools are governed by the Principles of Shakhsiyah Education, which have been developed through extensive research in order to generate a 21st century Islamic educational model that is true to the Qur’an and sunnah."
-        title="Shakhsiyah Schools"
-      />
-      <BlogDetailsPageMain />
-    </Layout>
-  )
-}
+const BlogDetailsPage = () => (
+  <Layout>
+    <BlogDetailsPageMain />
+  </Layout>
+)
 
 export default BlogDetailsPage
 
 export const Head = () => (
-  <SEO />
+  <Seo />
 )
