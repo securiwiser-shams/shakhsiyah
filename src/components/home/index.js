a7e61bef9b244f49030ac1c21a349887b33d0663
murabiyoon1 <info@murabiyoon.com>
2023-03-16 12:46:35 +0000

    update

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -8,32 +8,25 @@ import AboutFeatureSection from './aboutFeatureSection';
 import AboutFeatureVideo from './aboutFeatureVideo';
 import PartnerSection from './partnerSection';
 import GallaryInstaSection from './gallaryInstaSection';
-import BrowserCourseSection from './browserCourseSection';
-import SeoComponent from "../seo"
+import BrowserCourseSection from './browserCourseSection';z
 
-const IndexPage = () => {
-  return (
-    <>
-      <SeoComponent 
-        title="Home Page"
-        description="This is the homepage of my Gatsby site"
-        // provide the correct image path here
-        image="/images/homepage.png"
-      />
-      <Header />
-      <main>
-        <HeroSection />
-        <UniversityCardSection />
-        <UniversityMessage />
-        <AboutFeatureSection />
-        <AboutFeatureVideo />
-        <BrowserCourseSection />
-        <PartnerSection />
-        <GallaryInstaSection />
-      </main>
-      <Footer />
-    </>
-  );
+const index = () => {
+    return (
+        <>
+        <Header />
+            <main>
+                <HeroSection />
+                <UniversityCardSection />
+                <UniversityMessage />
+                <AboutFeatureSection />
+                <AboutFeatureVideo />
+                <BrowserCourseSection />
+                <PartnerSection />
+                <GallaryInstaSection />
+            </main>
+            <Footer />
+        </>
+    );
 };
 
-export default IndexPage;
+export default index;
--- src/components/homeThree/index.js
+++ src/components/homeThree/index.js
@@ -10,31 +10,25 @@ import GallaryInstaSection from './gallaryInstaSection';
 import HeroSectionThree from './heroSectionThree';
 import UniversityCardSection from './universityCardSection';
 import UniversityMessage from './universityMessage';
-import SeoComponent from "../seo"
 
-const IndexPage = () => {
+const index = () => {
     return (
-      <>
-        <SeoComponent 
-          title="Home Page"
-          description="This is the homepage of my Gatsby site"
-          // provide the correct image path here
-          image="/images/homepage.png"
-        />
-        <HeaderThree />
-        <main>
-          <HeroSectionThree />
-          <UniversityCardSection />
-          <UniversityMessage />
-          <AcademicCourse />
-          <CounterSectionThree />
-          <CampusSection />
-          <EventSection />
-          <BlogSection />
-          <GallaryInstaSection />
-        </main>
-        <FooterTwo />
-      </>
+        <>
+            <HeaderThree />
+            <main>
+                <HeroSectionThree />
+                <UniversityCardSection />
+                <UniversityMessage />
+                <AcademicCourse />
+                <CounterSectionThree />
+                <CampusSection />
+                <EventSection />
+                <BlogSection />
+                <GallaryInstaSection />
+            </main>
+            <FooterTwo />
+        </>
     );
-  };
-  
+};
+
+export default index;
