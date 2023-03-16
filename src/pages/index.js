b023619bd70e7252f28a55b003f4b63cf984da68
murabiyoon1 <info@murabiyoon.com>
2023-03-16 16:25:46 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,32 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Seo } from "../components/seo"
-import HomeMain from "../components/home"
 
-const IndexPage = () => (
-  <Layout>
-    <HomeMain />
-  </Layout>
-)
+export const query = graphql`
+  query {
+    allFile(
+      filter: {sourceInstanceName: {eq: "seo-images"}, name: {in: "index"}}
+    ) {
+      edges {
+        node {
+          publicURL
+        }
+      }
+    }
+  }
+`
+const IndexPage = ({ data }) => {
+  const seoImageUrl = data?.allFile?.edges[0]?.node?.publicURL
+  return (
+    <Layout>
+      <Seo
+        image={seoImageUrl?.length > 1 ? `https://shakhsiyah.netlify.app${seoImageUrl}` : null}
+        description="Shakhsiyah Schools are governed by the Principles of Shakhsiyah Education, which have been developed through extensive research in order to generate a 21st century Islamic educational model that is true to the Qur’an and sunnah."
+        title="Shakhsiyah Schools"
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage
-
-export const Head = () => (
-  <Seo />
-)
