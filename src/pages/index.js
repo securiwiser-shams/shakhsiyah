cba2f65ef85778e05b27c266197743e8654b7a03
murabiyoon1 <info@murabiyoon.com>
2023-03-16 16:51:53 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import HomeMain from "../components/home"
-import { Seo } from "../components/layout/seo"
+import { SEO } from "../components/layout/seo"
 
 export const query = graphql`
   query {
@@ -21,7 +21,7 @@ const IndexPage = ({ data }) => {
   const seoImageUrl = data?.allFile?.edges[0]?.node?.publicURL
   return (
     <Layout>
-      <Seo
+      <SEO
         image={seoImageUrl?.length > 1 ? `https://shakhsiyah.netlify.app${seoImageUrl}` : null}
         description="Shakhsiyah Schools are governed by the Principles of Shakhsiyah Education, which have been developed through extensive research in order to generate a 21st century Islamic educational model that is true to the Qur’an and sunnah."
         title="Shakhsiyah Schools"
