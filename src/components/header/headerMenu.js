6a6c4940d9f2d17e03b80ca5ae25e3e60f5785d6
murabiyoon1 <info@murabiyoon.com>
2023-03-19 13:04:00 +0000

    update

--- src/components/header/headerMenu.js
+++ src/components/header/headerMenu.js
@@ -3,67 +3,140 @@ import React from 'react';
 
 const HeaderMenu = () => {
     return (
-        <div id="lazy-load-container">
-            <ul>
-                <li><Link to="/principle">About Us</Link>
-                    <ul className="sub-menu">
-                        <li><Link to="/principles">Principles</Link></li>
-                        <li><Link to="/mission-statement">Mission Statement</Link></li>
-                        <li className="menu-item-has-children"><Link to="#">Team</Link>
-                            <ul className="sub-menu">
-                                <li><Link to="/trustees">Trustees</Link></li>
-                                <li><Link to="/senior-team">Senior Leadership Team</Link></li>
-                                <li><Link to="/safeguarding-team">Safeguarding Team</Link></li>
-                            </ul>
-                        </li>
-                        <li><Link to="/policies">Policies</Link></li>
-                        <li><Link to="/vacancies">Vacancies</Link></li>
-                        <li><Link to="/parents">Information for Parent</Link></li>
+        <ul>
+            <li className="menu-item-has-children"><Link to="#">About Us</Link>
+                <ul className="sub-menu">
+                    <li><Link to="/principles">Principles</Link></li>
+                    <li><Link to="/mission-statement">Mission Statement</Link></li>
+                    <li className="menu-item-has-children"><Link to="#">Team</Link>
+                        <ul className="sub-menu">
+                            <li><Link to="/trustees">Trustees</Link></li>
+                            <li><Link to="/senior-team">Senior Leadership Team</Link></li>
+                            <li><Link to="/safeguarding-team">Safeguarding Team</Link></li>
+                        </ul>
+                    </li>
+                    <li><Link to="/policies">Policies</Link></li>
+                    <li><Link to="/vacancies">Vacancies</Link></li>
+                    <li><Link to="/parents">Information for Parent</Link></li>
                     </ul>
-                </li>
-                <li><Link to="/admissions">Admissions</Link></li>
-                <li><Link to="/early-curriculum">Early Years</Link>
-                    <ul className="sub-menu">
-                        <li><Link to="/early-curriculum">Curriculum</Link></li>
-                        <li><Link to="/early-assessment">Assessment</Link></li>
-                        <li><Link to="/school-life-early">School Life</Link></li>
-                        <li><Link to="#">Term Dates</Link></li>
-                        <li><Link to="#">Calendar</Link></li>
+            </li>
+            <li className="menu-item-has-children"><Link to="/admissions">Admissions</Link>
+            </li>
+            <li className="menu-item-has-children"><Link to="#">Early Years</Link>
+                <ul className="sub-menu">
+                    <li><Link to="/early-curriculum">Curriculum</Link></li>
+                    <li><Link to="/early-assessment">Assessment</Link></li>
+                    <li><Link to="/school-life-early">School Life</Link></li>
+                    <li><Link to="#">Term Dates</Link></li>
+                    <li><Link to="#">Calendar</Link></li>
+                </ul>
+            </li>
+            <li className="menu-item-has-children"><Link to="#">Primary School</Link>
+                <ul className="sub-menu">
+                <li><Link to="/primary-curriculum">Curriculum</Link></li>
+                    <li><Link to="/primary-assessment">Assessment</Link></li>
+                    <li><Link to="/school-life-primary">School Life</Link></li>
+                    <li><Link to="#">Term Dates</Link></li>
+                    <li><Link to="#">Calendar</Link></li>
+                </ul>
+            </li>
+            <li className="menu-item-has-children"><Link to="#">Secondary School</Link>
+                <ul className="sub-menu">
+                <li><Link to="/secondary-curriculum">Curriculum</Link></li>
+                <li><Link to="#">Assessment</Link></li>
+                <li><Link to="/school-life-secondary">School Life</Link></li>
+                <li><Link to="#">Term Dates</Link></li>
+                <li><Link to="#">Calendar</Link></li> 
+            </ul>
+            </li>
+            <li className="menu-item-has-children"><Link to="#">ISF Community</Link>
+                <ul className="sub-menu">
+                    <li><Link to="/family-halaqah">Family Halaqah</Link></li>
+                    <li><Link to="/webinars">Webinars</Link></li>
+                    <li className="menu-item-has-children"><Link to="#">Events</Link>
+                        <ul className="sub-menu">
+                        <li><Link to="/event">Upcoming Events</Link></li>
+                        <li><Link to="/event-details">Tour De Shakhsiyah</Link></li>
+                        </ul>
+                    </li>
+                    <li><Link to="/principles">Announcements</Link></li>
                     </ul>
-                </li>
-                <li><Link to="/primary-curriculum">Primary School</Link>
-                    <ul className="sub-menu">
-                        <li><Link to="/primary-curriculum">Curriculum</Link></li>
-                        <li><Link to="/primary-assessment">Assessment</Link></li>
-                        <li><Link to="/school-life-primary">School Life</Link></li>
-                        <li><Link to="#">Term Dates</Link></li>
-                        <li><Link to="#">Calendar</Link></li>
+            {/*</li>
+            <li className="menu-item-has-children"><Link to="/shop">Announcements</Link>
+                <ul className="sub-menu">
+                    <li><Link to="/shop">Shop</Link></li>
+                    <li><Link to="/shop-details">Shop Details</Link></li>
+                    <li><Link to="/wishlist">wishlist</Link></li>
+                    <li><Link to="/cart">cart</Link></li>
+                    <li><Link to="/checkout">checkout</Link></li>
                     </ul>
-                </li>
-                <li><Link to="/secondary-curriculum">Secondary School</Link>
-                    <ul className="sub-menu">
-                        <li><Link to="/secondary-curriculum">Curriculum</Link></li>
-                        <li><Link to="#">Assessment</Link></li>
-                        <li><Link to="/school-life-secondary">School Life</Link></li>
-                        <li><Link to="#">Term Dates</Link></li>
-                        <li><Link to="#">Calendar</Link></li>
-                    </ul>
-                </li>
-                <li><Link to="/family-halaqah">ISF Community</Link>
-                    <ul className="sub-menu">
-                        <li><Link to="/family-halaqah">Family Halaqah</Link></li>
-                        <li><Link to="/webinars">Webinars</Link></li>
-                        <li className="menu-item-has-children"><Link to="#">Events</Link>
-                            <ul className="sub-menu">
-                                <li><Link to="/event">Upcoming Events</Link></li>
-                                <li><Link to="/event-details">Tour De Shakhsiyah</Link></li>
-                                </ul>
-                                </li>
-                                <li><Link to="/announcements">Announcements</Link></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  );
+            </li>
+            <li className="menu-item-has-children"><Link to="/shop">Family Halaqah</Link>
+                <ul className="sub-menu">
+                    <li><Link to="/shop">Shop</Link></li>
+                    <li><Link to="/shop-details">Shop Details</Link></li>
+                    <li><Link to="/wishlist">wishlist</Link></li>
+                    <li><Link to="/cart">cart</Link></li>
+                    <li><Link to="/checkout">checkout</Link></li>
+                </ul>
+            </li>
+            <li className="menu-item-has-children"><Link to="/course">Course</Link>
+                <ul className="sub-menu">
+                    <li><Link to="/course">Course 1</Link></li>
+                    <li><Link to="/courseTwo">Course 2</Link></li>
+                    <li><Link to="/courseThree">Course 3</Link></li>
+                    <li><Link to="/courseFour">Course 4</Link></li>
+                    <li><Link to="/course-details">Course Details</Link></li>
+                    <li><Link to="/webinars">Webinars</Link></li>
+                    <li><Link to="/webinar-details">Webinar Details</Link></li>
+                </ul>
+            </li>
+            <li className="menu-item-has-children"><a href="#!">Pages</a>
+                <ul className="sub-menu">
+                    <li><Link to="/about">About</Link></li>
+                    <li className="menu-item-has-children"><Link to="/instructor">Team</Link>
+                        <ul className="sub-menu">
+                            <li><Link to="/instructor">Trustees</Link></li>
+                            <li><Link to="/senior-team">Senior Leadership Team</Link></li>
+                            <li><Link to="/safeguarding-team">Safeguarding Team</Link></li>
+                            <li><Link to="/instructor-profile">instructor profile</Link></li>
+                            <li><Link to="/student-profile">Student profile</Link></li>
+                            <li><Link to="/become-instructor">become instructor</Link></li>
+                        </ul>
+                    </li>
+                    <li className="menu-item-has-children"><Link to="/zoom-class">zoom class</Link>
+                        <ul className="sub-menu">
+                            <li><Link to="/zoom-class">zoom class</Link></li>
+                            <li><Link to="/zoom-class-details">zoom class details</Link></li>
+                        </ul>
+                    </li>
+                    <li className="menu-item-has-children"><Link to="/blog">Blog</Link>
+                        <ul className="sub-menu">
+                            <li><Link to="/blog">Blog</Link></li>
+                            <li><Link to="/blog-details">blog details</Link></li>
+                        </ul>
+                    </li>
+                    <li className="menu-item-has-children"><Link to="/event">Event</Link>
+                        <ul className="sub-menu">
+                            <li><Link to="/event">Event</Link></li>
+                            <li><Link to="/event-details">Event Details</Link></li>
+                        </ul>
+                    </li>
+                    <li className="menu-item-has-children"><Link to="/faq-page">FAQ page</Link>
+                        <ul className="sub-menu">
+                            <li><Link to="/faq-page">FAQ page</Link></li>
+                            <li><Link to="/faq-details">FAQ details</Link></li>
+                        </ul>
+                    </li>
+                    <li><Link to="/membership">membership plan</Link></li>
+                    <li><Link to="/login">Sign In</Link></li>
+                    <li><Link to="/registration">Sign Up</Link></li>
+                    <li><Link to="/404">404-page</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+    </ul>*/}
+            </li>
+        </ul>
+    );
 };
+
 export default HeaderMenu;
