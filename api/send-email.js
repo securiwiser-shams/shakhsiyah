37034942c64e5a08b226b9ff7533338e7bf45c3c
Shamsul Islam <abuzaynab1@hotmail.co.uk>
2023-03-20 02:01:42 +0000

    update

--- api/send-email.js
+++ api/send-email.js
@@ -1,46 +1,39 @@
 const mail = require('@sendgrid/mail');
-mail.setApiKey(process.env.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);
+mail.setApiKey('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');
 
 export default function handler(req, res) {
   try {
+    const body = JSON.parse(req.body);
+
     const message = {
-      from: process.env.SENDGRID_AUTHORIZED_EMAIL,
+      from: 'shams@securiwiser.com',
+      to: 'shams@securiwiser.com',
+      subject: body.title,
+      html: `<div>
+        <p><strong>Name: </strong> ${body.name}</p>
+        <p><strong>Email: </strong>${body.email}</p>
+        <p><strong>${body.school ? 'School name: ' : 'Subject: '}</strong>${body.school ? body.school : body.subject}</p>
+        <p><strong>Message: </strong><br/>${body.message}</p>
+      </div>`
     };
 
-    const body = req.body;
-
-    const content = `<div>
-      <p><strong>Name: </strong>${body.name}</p>
-      <p><strong>Email: </strong>${body.email}</p>
-      <p><strong>${body.school ? 'School name: ' : 'Subject: '}</strong>${
-      body.school ? body.school : body.subject
-    }</p>
-      <p><strong>Message: </strong><br/>${body.message}</p>
-    </div>`;
-
-    if (body) {
-      message.to = 'shams@securiwiser.com';
-      message.subject = body.title;
-      message.html = content;
-    }
-
-    mail.send(message).then(
+    return mail.send(message).then(
       () => {
         res.status(200).json({
-          message: 'Email sent successfully!',
-        });
+          message: "Email sent successfully",
+        })
       },
-      (error) => {
-        console.error(error);
+      error => {
+        console.error(error)
         if (error.response) {
           return res.status(500).json({
             error: error.response,
-          });
+          })
         }
       }
     );
   } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: 'There was an error', error: err });
+    console.log(err)
+    return res.status(500).json({ message: "There was an error", error: err })
   }
 }
