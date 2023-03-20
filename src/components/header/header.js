d2435eba7e8a48074b3b154a8cef1a5ffa93d23e
murabiyoon1 <info@murabiyoon.com>
2023-03-20 10:10:41 +0000

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,86 +6,86 @@ import HeaderTop from './headerTop';
 import MobileMenu from './mobileMenu';
 
 const HeaderFour = () => {
-    const { sticky } = useSticky();
+  const { sticky } = useSticky();
 
-    const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
-    return (
-        <header>
-          <HeaderTop />
-          <div className="header-top-area d-none d-lg-block">
-            <div className="container-fluid p-0 header-main-wrapper-background">
-              <div className="header-top-inner">
-                <div className="row align-items-center">
-                  <div className="col-xl-8 col-lg-8"></div>
-                  <div className="col-xl-4 col-lg-4">
-                    <div className="header-top-login d-flex f-right"></div>
+  return (
+    <header>
+      <HeaderTop />
+      <div className="header-top-area d-none d-lg-block">
+        <div className="container-fluid p-0 header-main-wrapper-background">
+          <div className="header-top-inner">
+            <div className="row align-items-center">
+              <div className="col-xl-8 col-lg-8"></div>
+              <div className="col-xl-4 col-lg-4">
+                <div className="header-top-login d-flex f-right"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={
+          sticky
+            ? 'sticky header-area sticky-header'
+            : 'header-area sticky-header'
+        }
+      >
+        <div className="container-fluid p-0 header-main-wrapper-background">
+          <div className="header-main-wrapper">
+            <div className="row align-items-center">
+              <div className="col-xl-12 col-lg-7 col-md-5 col-sm-9 col-9">
+                <div className="header-left d-flex align-items-center">
+                  <Link to="/">
+                    <img
+                      src="/assets/img/logo/logo.svg"
+                      alt="logo"
+                      style={{ width: "211px", height: "71px" }}
+                    />
+                  </Link>
+                  <div className="main-menu d-none d-xl-block">
+                    <nav id="mobile-menu">
+                      <HeaderMenu />
+                    </nav>
+                  </div>
+                  <Link
+                    to="/admissions"
+                    className="user-btn-sign-up edu-btn ml-auto apply-button"
+                    style={{ width: "100px", height: "40px", marginLeft: "20px" }}
+                  >
+                    Apply
+                  </Link>
+                </div>
+              </div>
+              <div className="col-xl-8 col-lg-5 col-md-7 col-sm-3 col-3">
+                <div className="header-right d-flex align-items-center justify-content-end">
+                  <div className="menu-bar d-xl-none ml-20">
+                    <button
+                      type="button"
+                      className="side-toggle"
+                      onClick={() => {
+                        setMenuOpen(!menuOpen);
+                      }}
+                      aria-label="Toggle side menu"
+                    >
+                      <div className="bar-icon"></div>
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-    
-          <div
-            className={
-              sticky
-                ? 'sticky header-area sticky-header'
-                : 'header-area sticky-header'
-            }
-          >
-            <div className="container-fluid p-0 header-main-wrapper-background">
-              <div className="header-main-wrapper">
-                <div className="row align-items-center">
-                  <div className="col-xl-12 col-lg-7 col-md-5 col-sm-9 col-9">
-                    <div className="header-left d-flex align-items-center">
-                      <Link to="/">
-                        <img
-                          src="/assets/img/logo/logo.svg"
-                          alt="logo"
-                          width="211"
-                          height="71"
-                        />
-                      </Link>
-                      <div className="main-menu d-none d-xl-block">
-                        <nav id="mobile-menu">
-                          <HeaderMenu />
-                        </nav>
-                      </div>
-                      <Link
-  to="/admissions"
-  className="user-btn-sign-up edu-btn ml-auto apply-button"
-  style={{ marginLeft: "20px" }}>
-  Apply
-</Link>
-                    </div>
-                  </div>
-                  <div className="col-xl-8 col-lg-5 col-md-7 col-sm-3 col-3">
-                    <div className="header-right d-flex align-items-center justify-content-end">
-                      <div className="menu-bar d-xl-none ml-20">
-                        <button
-                          type="button"
-                          className="side-toggle"
-                          onClick={() => {
-                            setMenuOpen(!menuOpen);
-                          }}
-                          aria-label="Toggle side menu"
-                        >
-                          <div className="bar-icon"></div>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        </div>
+      </div>
 
+      <div style={{ display: menuOpen ? "block" : "none", width: "280px" }}></div>
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div onClick={() => setMenuOpen(false)} className={menuOpen ? "offcanvas-overlay overlay-signin" : "offcanvas-overlay"}></div>
 
-            <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-            <div onClick={() => setMenuOpen(false)} className={menuOpen ? "offcanvas-overlay overlay-signin" : "offcanvas-overlay"}></div>
-
-            </header>
-    );
+    </header>
+  );
 };
 
 export default HeaderFour;
