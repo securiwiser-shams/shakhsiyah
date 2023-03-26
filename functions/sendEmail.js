7490046867ef393eda6e2d17a7f328678816615a
murabiyoon1 <info@murabiyoon.com>
2023-03-26 16:15:59 +0100

    Update sendEmail.js

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,24 +1,28 @@
 const sgMail = require('@sendgrid/mail');
 
 exports.handler = async (event) => {
-  const { name, phone, email, subject, message } = JSON.parse(event.body);
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-  const msg = {
-    to: 'shams@securiwiser.com',
-    from: 'shams@securiwiser.com',
-    subject: subject,
-    text: `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\n\nMessage: ${message}`,
-  };
-
   try {
+    const { name, phone, email, subject, message } = JSON.parse(event.body);
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+      to: 'shams@securiwiser.com',
+      from: 'shams@securiwiser.com',
+      subject: subject,
+      text: `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\n\nMessage: ${message}`,
+    };
+
     await sgMail.send(msg);
+    
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Email sent successfully' }),
     };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.toString() }),
+    };
   }
 };
