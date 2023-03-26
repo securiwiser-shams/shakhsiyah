7e23815c0e995cb1b0389ea08d9428c545865681
murabiyoon1 <info@murabiyoon.com>
2023-03-26 17:50:22 +0100

    Update sendEmail.js

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
 
     const msg = {
       to: 'shams@securiwiser.com',
-      from: 'shams@securiwiser.com',
+      from: process.env.SENDGRID_AUTHORIZED_EMAIL,
       subject: subject,
       text: `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\n\nMessage: ${message}`,
     };
