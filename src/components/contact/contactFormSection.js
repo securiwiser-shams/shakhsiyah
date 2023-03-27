314c6a46a85f53666934dca59d3196b794bbcb2a
murabiyoon1 <info@murabiyoon.com>
2023-03-27 12:34:58 +0100

    Update contactFormSection.js

--- src/components/contact/contactFormSection.js
+++ src/components/contact/contactFormSection.js
@@ -12,49 +12,55 @@ const ContactFormSection = () => {
 
   const [formErrors, setFormErrors] = useState({});
 
+  const [showModal, setShowModal] = useState(false);
+
   const { name, phone, email, subject, message } = formData;
 
   const handleChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-    
-      // Validate form data
-      const errors = {};
-      if (!name) errors.name = 'Name is required';
-      if (!phone) errors.phone = 'Phone number is required';
-      if (!email) errors.email = 'Email is required';
-      if (!subject) errors.subject = 'Subject is required';
-      if (!message) errors.message = 'Message is required';
-    
-      if (Object.keys(errors).length > 0) {
-        setFormErrors(errors);
-        return;
-      }
-    
-      try {
-        const res = await axios.post('/.netlify/functions/sendEmail', {
-          name,
-          phone,
-          email,
-          subject,
-          message,
-        });
-        console.log(res.data);
-        setFormErrors({});
-        setFormData({
-          name: '',
-          phone: '',
-          email: '',
-          subject: '',
-          message: '',
-        });
-        window.alert('Message sent successfully!');
-      } catch (err) {
-        console.error(err);
-      }
-    };    
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate form data
+    const errors = {};
+    if (!name) errors.name = 'Name is required';
+    if (!phone) errors.phone = 'Phone number is required';
+    if (!email) errors.email = 'Email is required';
+    if (!subject) errors.subject = 'Subject is required';
+    if (!message) errors.message = 'Message is required';
+
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      return;
+    }
+
+    try {
+      const res = await axios.post('/.netlify/functions/sendEmail', {
+        name,
+        phone,
+        email,
+        subject,
+        message,
+      });
+      console.log(res.data);
+      setFormErrors({});
+      setFormData({
+        name: '',
+        phone: '',
+        email: '',
+        subject: '',
+        message: '',
+      });
+      setShowModal(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <div className='contact-form'>
@@ -118,19 +124,6 @@ const ContactFormSection = () => {
               </select>
               {formErrors.subject && (
                 <span className='error'>{formErrors.subject}</span>
-              )}
-            </div>
-          </div>
-          <div className='col-xl-12'>
-            <div className='contact-from-input mb-20'>
-              <textarea
-                placeholder='Message'
-                name='message'
-                value={message}
-                onChange={handleChange}
-              ></textarea>
-              {formErrors.message && (
-                <span className='error'>{formErrors.message}</span>
               )}
             </div>
           </div>
