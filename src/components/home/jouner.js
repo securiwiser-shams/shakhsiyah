ae26e14d2dc9bc13a31f7d96f9fa3d66fede5201
mgnani <93810026+mgnani@users.noreply.github.com>
2023-03-30 13:25:19 -0300

    check
    check

--- src/components/home/jouner.js
+++ src/components/home/jouner.js
@@ -9,41 +9,49 @@ const JounerSection = () => {
     const yearActual = fechaActual.getFullYear();
     const yearMenos7 = yearActual - 17;
     const [isDisabled, setIsDisabled] = useState(true);
-    const [selectedDay, setSelectedDay] = useState('');
-    const [selectedMonth, setSelectedMonth] = useState('');
-    const [selectedYear, setSelectedYear] = useState('');
 
     function handleDisableClick() {
-        setIsDisabled(!isDisabled);
-    }
-
-    function handleFormSubmit(event) {
-        event.preventDefault();
-        if (isNaN(selectedDay) || isNaN(selectedMonth) || isNaN(selectedYear)) {
+        var day = document.querySelector(".day");
+        var month = document.querySelector(".month");
+        var year = document.querySelector(".year");
+        if( (day.innerHTML.includes('DD') || month.innerHTML.includes('MM') || year.innerHTML.includes('YY')) || isDisabled == false){
             setIsDisabled(true);
-            alert('Please enter a valid input');
-        } else {
+            if(day.innerHTML.includes('DD')){
+                day.style.color = 'red';
+            }
+            if(month.innerHTML.includes('MM')){
+                month.style.color = 'red';
+            }
+            if(year.innerHTML.includes('YY')){
+                year.style.color = 'red';
+            }
+        }else{
             setIsDisabled(false);
+            day.style.color = 'black';
+            month.style.color = 'black';
+            year.style.color = 'black';
+        }
+        if(isDisabled == false){
+            setIsDisabled(true);
         }
     }
-
     return (
         <>
             <div className='jouney_container'>
                 <div className='jouney_content'>
                     <h1>Start Your Journey</h1>
                     <p>Enter your child’s date of birth to discover the key steps to joining us </p>
-                    <form onSubmit={handleFormSubmit}>
-                        <RangoNumerico rango={[1, 31]} date="DD" onChange={setSelectedDay} />
-                        <RangoNumerico rango={[1, 12]} date="MM" onChange={setSelectedMonth} />
-                        <RangoNumerico rango={[yearActual, yearMenos7]} date="YY" onChange={setSelectedYear} />
-                        <div className='button_arrow' onClick={handleFormSubmit}>
+                    <div className="date_container">
+                        <RangoNumerico rango={[1, 31]} date="DD" selector="day"/>
+                        <RangoNumerico rango={[1, 12]} date="MM" selector="month"/>
+                        <RangoNumerico rango={[yearActual, yearMenos7]} date="YY" selector="year"/>
+                        <div className='button_arrow' onClick={handleDisableClick}>
                             <img src={arrow_gold} />
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-            <div className={`adm-async-form-two ${isDisabled ? "disable" : ""}`} >
+            <div className={`adm-async-form-two ${isDisabled ? "disable" : "alive"}`} >
                 <div className='adm-results-slide-out'>
                     <div className="close-adm-results" onClick={handleDisableClick}></div>
                     <Jouney_content/>
--- src/components/home/range.js
+++ src/components/home/range.js
@@ -1,53 +1,46 @@
 import React, { useState } from "react";
-import arrow_down from '../../../static/assets/img/arrow/arrow_down.svg'
+import arror_dowm from '../../../static/assets/img/arrow/arrow_down.svg'
 
 function RangoNumerico(props) {
-  const [divs, setDivs] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState('');
-  const [isValid, setIsValid] = useState(true);
-
-  function generarDivs() {
-    const num1 = props.rango[0];
-    const num2 = props.rango[1];
-    const rango = Math.abs(num1 - num2);
-    const cantidadDivs = rango + 1;
-    const numerosEnRango = Array.from({ length: cantidadDivs }, (_, index) => {
-      return num1 < num2 ? num1 + index : num1 - index;
-    });
-    const divsGenerados = numerosEnRango.map((numero) => (
-      <div
-        key={numero}
-        className="adm-select"
-        data-val={numero.toString().padStart(2, "0")}
-        onClick={() => {
-          setSelectedValue(numero);
-          setIsOpen(false);
-        }}
-      >
-        {numero}
+    const [divs, setDivs] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedValue, setSelectedValue] = useState('');
+  
+    function generarDivs() {
+      const num1 = props.rango[0];
+      const num2 = props.rango[1];
+      const rango = Math.abs(num1 - num2);
+      const cantidadDivs = rango + 1;
+      const numerosEnRango = Array.from({ length: cantidadDivs }, (_, index) => {
+        return num1 < num2 ? num1 + index : num1 - index;
+      });
+      const divsGenerados = numerosEnRango.map((numero) => (
+        <div
+          key={numero}
+          className="adm-select"
+          data-val={numero.toString().padStart(2, "0")}
+          onClick={() => {
+            document.querySelector(".date_title").style.color = 'black';
+            setSelectedValue(numero);
+            setIsOpen(false);
+          }}
+        >
+          {numero}
+        </div>
+      ));
+      setDivs(divsGenerados);
+      setIsOpen(!isOpen);
+    }
+  
+    return (
+      <div>
+        <div onClick={generarDivs} className={`date_title ${isOpen ? 'best_border' : ''}`}>
+          <label><p className={props.selector}>{selectedValue || props.date} </p><img src={arror_dowm}/></label>
+        </div>
+        <div className={isOpen ? 'open' : 'date_Seleccion'}>{divs}</div>
       </div>
-    ));
-    setDivs(divsGenerados);
-    setIsOpen(!isOpen);
+    );
   }
-
-  function handleInputChange(event) {
-    const value = event.target.value;
-    const regex = /^[0-9]*$/;
-    setIsValid(regex.test(value));
-    setSelectedValue(value);
-  }
-
-  return (
-    <div>
-      <div onClick={generarDivs} className={`date_title ${isOpen ? 'best_border' : ''}`}>
-        <label style={{color: isValid ? 'black' : 'red'}}>{selectedValue || props.date} <img src={arrow_down} /></label>
-      </div>
-      <div className={isOpen ? 'open' : 'date_Seleccion'}>{divs}</div>
-      {!isValid && <div style={{color: 'red'}}>Please enter a valid input</div>}
-    </div>
-  );
-}
-
-export default RangoNumerico;
+  
+  export default RangoNumerico;
+  
