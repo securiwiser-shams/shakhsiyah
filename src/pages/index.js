6adbc05e3edbc72f1f36eb8670e2a73868dda7ea
murabiyoon1 <info@murabiyoon.com>
2023-03-31 11:33:14 +0100

    cookies removal

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,10 +30,6 @@ const IndexPage = ({ data }) => {
         title="Shakhsiyah Schools"
       />
       <HomeMain />
-      <CookieNotice>
-    <h4>This websites uses cookies.</h4>
-    <p>We use cookies to make the site work better, but also to see how you interact with it. how you interact with it. We will only use cookies if you allow us to do so by clicking by clicking on "Accept Cookies". You can also choose which cookie you want to allow.</p>
-</CookieNotice>
     </Layout>
   );
 };
