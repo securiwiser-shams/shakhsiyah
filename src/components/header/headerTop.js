6a53a9220f7415a1d50fb04d4ad9e9d776be3b38
mohamed salbi <mohamedsali602@live.com>
2023-04-02 22:29:26 +0000

    second step done for the header render nothing untill its loaded

--- src/components/header/headerTop.js
+++ src/components/header/headerTop.js
@@ -17,62 +17,51 @@ const HeaderTop = () => {
 
   const headerContent = data.allWpPost.nodes[0]?.content || '';
   const [parsedContent, setParsedContent] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const parseHtmlString = (htmlString) => {
-      const htmlDoc = parse(htmlString);
-      const content = htmlDoc.childNodes;
-      const parsedContent = Array.from(content);
-
-      const renderElement = (element, index) => {
-        if (element.nodeType === 3) { // check if it's a text node
-          return element.rawText;
-        }
-        if (element.nodeType === 1) { // check if it's an element node
-          const tagName = element.tagName.toLowerCase();
-          const children = Array.from(element.childNodes);
+      const root = parse(htmlString);
+      const parsedContent = root.childNodes.map(node => {
+        if (node.nodeType === 1) { // Node.ELEMENT_NODE
+          const tagName = node.tagName.toLowerCase();
           if (tagName === 'strong') {
-            return (
-              <strong key={index}>
-                {children.map((child, childIndex) => renderElement(child, childIndex))}
-              </strong>
-            );
+            return <strong key={node.id}>{node.text}</strong>;
           }
           if (tagName === 'code') {
-            return (
-              <span key={index} >
-                {children.map((child, childIndex) => renderElement(child, childIndex))}
-              </span>
-            );
+            return <span key={node.id}>{node.text}</span>;
           }
           if (tagName === 'p') {
-            return (
-              <p key={index}>
-                {children.map((child, childIndex) => renderElement(child, childIndex))}
-              </p>
-            );
+            return <p key={node.id}>{node.text}</p>;
           }
+        } else if (node.nodeType === 3) { // Node.TEXT_NODE
+          return node.text;
         }
         return null;
-      };
+      }).filter(node => node !== null);
 
-      return parsedContent.map((node, index) => renderElement(node, index));
+      return parsedContent;
     };
 
-    setParsedContent(parseHtmlString(headerContent));
+    const parsed = parseHtmlString(headerContent);
+    setParsedContent(parsed);
+    setLoading(false);
   }, [headerContent]);
 
   const [isActiveF, setActiveF] = useState(true);
-
   const handleToggleF = () => {
     setActiveF(!isActiveF);
   };
+
+  if (loading) {
+    return null; // render nothing while loading
+  }
 
   return (
     <div className={`header-note-area p-relative d-none d-md-block ${isActiveF ? "eduman-header-notice-visible" : "eduman-header-notice-hide"}`}>
       <div className="container-fluid">
         <div className="note-text text-center">
-          {parsedContent} 
+          {parsedContent}
         </div>
       </div>
       <div className="eduman-header-notice-action-close">
