724af5ad5c76be5bf8b8b173b3f0c216da57bd4a
mohamed salbi <mohamedsali602@live.com>
2023-04-02 20:36:01 +0000

    reverting back

--- src/components/contact/contactMap1.js
+++ src/components/contact/contactMap1.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactMap = () => {
     return (
         <div className="google-map-area contact-map pt-100 mb-30">
-            <h3>North Branch - Map</h3> {/* Added h3 title */}
+            <h3>North London Branch - Map</h3> {/* Added h3 title */}
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.3146288234557!2d-0.08712088422732708!3d51.580796779647265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761c0daa1fac39%3A0x33777909b76851d5!2s1st%20Floor%2C%20277%20St%20Ann&#39;s%20Rd%2C%20London%20N15%205RG!5e0!3m2!1sen!2suk!4v1680006640111!5m2!1sen!2suk"></iframe>
         </div>
     ); 
--- src/components/header/headerTop.js
+++ src/components/header/headerTop.js
@@ -1,101 +1,25 @@
 import React, { useState } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 
 const HeaderTop = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allWpPost(filter: { title: { eq: "Header" } }) {
-        nodes {
-          id
-          title
-          content
-        }
-      }
-    }
-  `);
+    const [isActiveF, setActiveF] = useState("false");
 
-  const headerContent = data.allWpPost.nodes[0]?.content || '';
-
-  const parseHtmlString = (htmlString) => {
-    if (typeof window !== 'undefined' && window.DOMParser) {
-      const parser = new DOMParser();
-      const htmlDoc = parser.parseFromString(htmlString, 'text/html');
-      const content = htmlDoc.body.childNodes;
-      const parsedContent = Array.from(content);
-
-      const renderElement = (element, index, key) => {
-        if (element.nodeType === Node.TEXT_NODE) {
-          return element.textContent;
-        }
-
-        if (element.nodeType === Node.ELEMENT_NODE) {
-          const tagName = element.tagName.toLowerCase();
-          const children = Array.from(element.childNodes);
-
-          if (tagName === 'strong') {
-            return (
-              <strong key={key}>
-                {children.map((child, childIndex) =>
-                  renderElement(child, childIndex, `${key}-child-${childIndex}`)
-                )}
-              </strong>
-            );
-          }
-
-          if (tagName === 'code') {
-            return (
-              <span key={key}>
-                {children.map((child, childIndex) =>
-                  renderElement(child, childIndex, `${key}-child-${childIndex}`)
-                )}
-              </span>
-            );
-          }
-
-          if (tagName === 'p') {
-            return (
-              <p key={key}>
-                {children.map((child, childIndex) =>
-                  renderElement(child, childIndex, `${key}-child-${childIndex}`)
-                )}
-              </p>
-            );
-          }
-        }
-
-        return null;
-      };
-
-      return parsedContent.map((node, index) =>
-        renderElement(node, index, `header-top-${index}`)
-      );
-    }
-
-    return null;
-  };
-
-  const [isActiveF, setActiveF] = useState(true);
-
-  const handleToggleF = () => {
-    setActiveF(!isActiveF);
-  };
-
-  const parsedContent = parseHtmlString(headerContent);
-
-  return (
-    <div className={` header-note-area p-relative d-none d-md-block ${isActiveF ? "eduman-header-notice-visible" : "eduman-header-notice-hide"}`}>
-      <div className="container-fluid">
-        <div className="note-text text-center">
-          {parsedContent}
+    const handleToggleF = () => {
+        setActiveF(!isActiveF);
+    };
+    return (
+        <div className={`header-note-area p-relative d-none d-md-block ${isActiveF ? "eduman-header-notice-visible" : "eduman-header-notice-hide"}`}>
+            <div className="container-fluid">
+                <div className="note-text text-center">
+                    <p><span>Ramadan Mubarak </span>to all, wish you all a happy, safe and <span>blessed</span> Ramadan</p>
+                </div>
+            </div>
+            <div className="eduman-header-notice-action-close">
+            <button onClick={handleToggleF} aria-label="Close notification">
+            <i className="fal fa-times"></i>
+            </button>
+            </div>
         </div>
-      </div>
-      <div className="eduman-header-notice-action-close">
-        <button onClick={handleToggleF} aria-label="Close notification">
-          <i className="fal fa-times"></i>
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HeaderTop;
