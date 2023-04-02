6e6227f5ebc8fa4a221866811bd97697867980c6
mohamed salbi <mohamedsali602@live.com>
2023-04-02 08:08:03 +0000

    fixing up build

--- src/components/header/headerTop.js
+++ src/components/header/headerTop.js
@@ -17,62 +17,76 @@ const HeaderTop = () => {
   const headerContent = data.allWpPost.nodes[0]?.content || '';
 
   const parseHtmlString = (htmlString) => {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(htmlString, 'text/html');
-    const content = htmlDoc.body.childNodes;
-    const parsedContent = Array.from(content);
-  
-    const renderElement = (element, index) => {
-      if (element.nodeType === Node.TEXT_NODE) {
-        return element.textContent;
-      }
-      if (element.nodeType === Node.ELEMENT_NODE) {
-        const tagName = element.tagName.toLowerCase();
-        const children = Array.from(element.childNodes);
-        if (tagName === 'strong') {
-          return (
-            <span key={index}>
-              {children.map((child, childIndex) => renderElement(child, childIndex))}
-            </span>
-          );
+    if (typeof window !== 'undefined' && window.DOMParser) {
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(htmlString, 'text/html');
+      const content = htmlDoc.body.childNodes;
+      const parsedContent = Array.from(content);
+
+      const renderElement = (element, index, key) => {
+        if (element.nodeType === Node.TEXT_NODE) {
+          return element.textContent;
         }
-        if (tagName === 'code') {
-          return (
-            < >
-              {children.map((child, childIndex) => renderElement(child, childIndex))}
-            </>
-          );
+
+        if (element.nodeType === Node.ELEMENT_NODE) {
+          const tagName = element.tagName.toLowerCase();
+          const children = Array.from(element.childNodes);
+
+          if (tagName === 'strong') {
+            return (
+              <strong key={key}>
+                {children.map((child, childIndex) =>
+                  renderElement(child, childIndex, `${key}-child-${childIndex}`)
+                )}
+              </strong>
+            );
+          }
+
+          if (tagName === 'code') {
+            return (
+              <span key={key}>
+                {children.map((child, childIndex) =>
+                  renderElement(child, childIndex, `${key}-child-${childIndex}`)
+                )}
+              </span>
+            );
+          }
+
+          if (tagName === 'p') {
+            return (
+              <p key={key}>
+                {children.map((child, childIndex) =>
+                  renderElement(child, childIndex, `${key}-child-${childIndex}`)
+                )}
+              </p>
+            );
+          }
         }
-        if (tagName === 'p') {
-          return (
-            <p key={index}>
-              {children.map((child, childIndex) => renderElement(child, childIndex))}
-            </p>
-          );
-        }
-      }
-      return null;
-    };
-  
-    return parsedContent.map((node, index) => renderElement(node, index));
+
+        return null;
+      };
+
+      return parsedContent.map((node, index) =>
+        renderElement(node, index, `header-top-${index}`)
+      );
+    }
+
+    return null;
   };
-  
-  
 
-  const parsedContent = parseHtmlString(headerContent);
-
-  const [isActiveF, setActiveF] = useState("false");
+  const [isActiveF, setActiveF] = useState(true);
 
   const handleToggleF = () => {
     setActiveF(!isActiveF);
   };
-  console.log(parsedContent);
+
+  const parsedContent = parseHtmlString(headerContent);
 
   return (
-    <div className={`header-note-area p-relative d-none d-md-block ${isActiveF ? "eduman-header-notice-visible" : "eduman-header-notice-hide"}`}>
+    <div className={` header-note-area p-relative d-none d-md-block ${isActiveF ? "eduman-header-notice-visible" : "eduman-header-notice-hide"}`}>
       <div className="container-fluid">
         <div className="note-text text-center">
-          {parsedContent} 
+          {parsedContent}
         </div>
       </div>
       <div className="eduman-header-notice-action-close">
